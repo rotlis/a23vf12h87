9152afc1cadfeddfb3e665741c9cb9d479af2525
crazy <v-muhammadfaisal@nbnco.com.au>
2017-04-03 21:46:56 +1000

    Updated db schema baseline to add some unique constraints, adjusted the column lengths, as well as inserted some initial data.

--- migrations/sqls/20170329115321-schema-baseline-up.sql
+++ migrations/sqls/20170329115321-schema-baseline-up.sql
@@ -1,15 +1,17 @@
 CREATE TABLE CiServerTypes (
   id int(11) NOT NULL AUTO_INCREMENT,
-  serverType VARCHAR(20),
+  serverType VARCHAR(20) UNIQUE,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO CiServerTypes (serverType) VALUES('GO');
 
 CREATE TABLE CiServers (
   id int(11) NOT NULL AUTO_INCREMENT,
   serverTypeId int(11) NOT NULL,
-  username VARCHAR(20) NOT NULL,
-  password VARCHAR(20) NOT NULL,
-  cctrayUrl VARCHAR(20) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(70) NOT NULL,
+  cctrayUrl VARCHAR(90) NOT NULL UNIQUE,
   PRIMARY KEY (id, cctrayUrl),
   FOREIGN KEY (serverTypeId) REFERENCES CiServerTypes(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -17,24 +19,27 @@ CREATE TABLE CiServers (
 CREATE TABLE PipeLines (
   id int(11) NOT NULL AUTO_INCREMENT,
   serverId int(11) NOT NULL,
-  pipeline VARCHAR(20) NOT NULL,
+  pipeline VARCHAR(50) NOT NULL,
   status VARCHAR(20) NOT NULL,
   lastUpdateAt DATE NOT NULL,
   PRIMARY KEY (id),
+  UNIQUE (serverId, pipeline),
   FOREIGN KEY (serverId) REFERENCES CiServers(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Patterns (
   id int(11) NOT NULL AUTO_INCREMENT,
-  pattern VARCHAR(20) NOT NULL,
+  pattern VARCHAR(30) NOT NULL UNIQUE,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO Patterns (pattern) VALUES('Scroll'), ('Steady');
 
 CREATE TABLE Devices (
   id int(11) NOT NULL AUTO_INCREMENT,
   patternId int(11),
   pipelineId int(11) NOT NULL,
-  mac VARCHAR(20) NOT NULL,
+  mac VARCHAR(20) NOT NULL UNIQUE,
   brightness VARCHAR(20),
   firmware VARCHAR(20),
   lastMessage VARCHAR(20),
